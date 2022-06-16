8e4119541d8405fe12baa7b39466cc7990fd6840
Robert Ferdarko <rferdarko@gmail.com>
2022-06-15 20:26:24 -0400

    First Commit

---
+++ InheritanceSample/src/main/java/org/example/p2/A.java
@@ -0,0 +1,8 @@
+package org.example.p2;
+
+public class A {
+    public int v1 = 5;
+    public int v2 = 10;
+    public int v3 = 15;
+    public int v4 = 20;
+}
---
+++ InheritanceSample/src/main/java/org/example/p3/B.java
@@ -0,0 +1,9 @@
+package org.example.p3;
+
+import org.example.p2.A; // Imports A Class
+
+public class B extends A {
+    // extends A and make features available to B
+    public int v5 = 100;
+    public int v6 = 101;
+}
